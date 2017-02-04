2ddbffe87fd38001ec9e0e666cccd51d6f02aa36
Dan Slocombe <ds4314@ic.ac.uk>
2017-02-04 15:35:31 +0000

    Move closes Monday scripts to its own source file and session list to data file

---
+++ data/sessionsList.js
@@ -0,0 +1,10 @@
+var sessionsList = [
+"watercolour portraits",
+"life drawing",
+"introduction to oils",
+"life drawing",
+"pencil drawing: 'drawing on the right side of the brain'",
+"life drawing with clay",
+"watercolours",
+"acrylics"
+];
